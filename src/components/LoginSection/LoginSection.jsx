7c1a190b8a89fd4535502a29a5d579aea25fdd24
Leillac88 <leillacarvalho88@gmail.com>
2025-05-24 11:12:18 -0300

    add corrections on logisSection by Hudson

--- src/components/LoginSection/LoginSection.jsx
+++ src/components/LoginSection/LoginSection.jsx
@@ -1,7 +1,9 @@
 import './LoginSection.css'
-import { FaFacebook } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import Sapatos from '../../../public/login.png'
+import Gmail from '../../assets/gmail.png'
+import Face from '../../assets/face.png'
+
+import Login1 from '../../../public/login1.png'
+import Login2 from '../../../public/login2.png'
 
 export function SectionLogin() {
     return (
@@ -11,21 +13,22 @@ export function SectionLogin() {
                     <form className="form">
                         <h1>Acesse sua conta</h1>
                         <p>Novo cliente? Então registre-se <a href="/register">aqui</a>.</p>
-                        <label htmlFor="login">Login *</label>
+                        <label htmlFor="login">Login*</label>
                         <input type="text" id="login" name="login" placeholder="Insira seu login ou email" required />
-                        <label htmlFor="password">Senha *</label>
+                        <label htmlFor="password">Senha*</label>
                         <input type="password" id="password" name="password" placeholder="Insira sua senha" required />
                         <a href="/forgot-password" className="forgot-password">Esqueci minha senha</a>
                         <button>Acessar a Conta</button>
                         <div className='login-rede-sociais'>
                             <span>Ou faça login com</span>
-                            <FcGoogle />
-                            <FaFacebook />
+                            <img src={Gmail} className='sapatos-login' alt="Gmail Icon" />
+                            <img src={Face} className='sapatos-login' alt="Facebook Icon" />
                         </div>
                     </form>
                 </div>
                 <div className='image-sapatos'>
-                    <img src={Sapatos} className='sapatos-login' alt="sapatos" />
+                    <img src={Login1} className='sapatos-login1' alt="sapatos" />
+                    <img src={Login2} className='sapatos-login2' alt="sapatos" />
                 </div>
             </section>
         </>
