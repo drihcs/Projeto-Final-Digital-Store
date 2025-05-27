b33cf68b8c897ae967a17601693cc7e657e561c7
Leillac88 <leillacarvalho88@gmail.com>
2025-05-27 09:38:39 -0300

    changes on user register css

--- src/components/UserRegister/UserRegister.jsx
+++ src/components/UserRegister/UserRegister.jsx
@@ -65,79 +65,80 @@ export function UserCad() {
             <div className="criar-conta">
                 <h3>Criar Conta</h3>
             </div>
-
-            <form onSubmit={handleCreateUser} className="form-container1">
-                <div className="title-pessoal">
-                    <p>Informações Pessoais</p>
-                </div>
-
-                <div className="form-input-label1">
-                    <span className="linha"></span>
-
-                    <div className="input-group">
-                        <label htmlFor="name">Nome Completo*</label>
-                        <input type="text" id="name" placeholder="Insira seu nome" required />
+            <div className="form-box">
+                <form onSubmit={handleCreateUser} className="form-container1">
+                    <div className="title-pessoal">
+                        <p>Informações Pessoais</p>
                     </div>
 
-                    <div className="input-group">
-                        <label htmlFor="number">CPF*</label>
-                        <input type="text" id="number" placeholder="Insira seu CPF" required />
+                    <div className="form-input-label1">
+                        <span className="linha"></span>
+
+                        <div className="input-group">
+                            <label htmlFor="name">Nome Completo*</label>
+                            <input type="text" id="name" placeholder="Insira seu nome" required />
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="number">CPF*</label>
+                            <input type="text" id="number" placeholder="Insira seu CPF" required />
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="email">E-mail*</label>
+                            <input type="text" id="email" placeholder="Insira seu email" required />
+                        </div>
+
+                        <div className="input-group">
+                            <label htmlFor="celular">Celular*</label>
+                            <input type="text" id="celular" placeholder="Insira seu celular" />
+                        </div>
                     </div>
 
-                    <div className="input-group">
-                        <label htmlFor="email">E-mail*</label>
-                        <input type="email" id="email" placeholder="Insira seu email" required />
+                    <div className="title-entrega">
+                        <p>Informações de Entrega</p>
                     </div>
 
-                    <div className="input-group">
-                        <label htmlFor="celular">Celular*</label>
-                        <input type="text" id="celular" placeholder="Insira seu celular" />
-                    </div>
-                </div>
+                    <div className="form-input-label2">
+                        <span className="linha"></span>
 
-                <div className="title-entrega">
-                    <p>Informações de Entrega</p>
-                </div>
+                        <div className="input-group">
+                            <label htmlFor="endereco">Endereço*</label>
+                            <input type="text" id="endereco" placeholder="Insira seu endereço" required />
+                        </div>
 
-                <div className="form-input-label2">
-                    <span className="linha"></span>
+                        <div className="input-group">
+                            <label htmlFor="bairro">Bairro*</label>
+                            <input type="text" id="bairro" placeholder="Insira seu bairro" required />
+                        </div>
 
-                    <div className="input-group">
-                        <label htmlFor="endereco">Endereço*</label>
-                        <input type="text" id="endereco" placeholder="Insira seu endereço" required />
-                    </div>
+                        <div className="input-group">
+                            <label htmlFor="cidade">Cidade*</label>
+                            <input type="text" id="cidade" placeholder="Insira sua cidade" required />
+                        </div>
 
-                    <div className="input-group">
-                        <label htmlFor="bairro">Bairro*</label>
-                        <input type="text" id="bairro" placeholder="Insira seu bairro" required />
-                    </div>
+                        <div className="input-group">
+                            <label htmlFor="cep">CEP*</label>
+                            <input type="text" id="cep" placeholder="Insira seu CEP" required />
+                        </div>
 
-                    <div className="input-group">
-                        <label htmlFor="cidade">Cidade*</label>
-                        <input type="text" id="cidade" placeholder="Insira sua cidade" required />
-                    </div>
-
-                    <div className="input-group">
-                        <label htmlFor="cep">CEP*</label>
-                        <input type="text" id="cep" placeholder="Insira seu CEP" required />
+                        <div className="input-group">
+                            <label htmlFor="complemento">Complemento</label>
+                            <input type="text" id="complemento" placeholder="Insira complemento" />
+                        </div>
                     </div>
 
-                    <div className="input-group">
-                        <label htmlFor="complemento">Complemento</label>
-                        <input type="text" id="complemento" placeholder="Insira complemento" />
+                    <div className="container-checkbox">
+                        <input className="custon-checkbox" type="checkbox" />
+                        <p className="paragraph">
+                            Quero receber por email ofertas e novidades das lojas da Digital Store.
+                            A frequência de envios pode variar de acordo com a interação do cliente.
+                        </p>
                     </div>
-                </div>
 
-                <div className="container-checkbox">
-                    <input className="custon-checkbox" type="checkbox" />
-                    <p className="paragraph">
-                        Quero receber por email ofertas e novidades das lojas da Digital Store.
-                        A frequência de envios pode variar de acordo com a interação do cliente.
-                    </p>
-                </div>
-
-                <button type="submit">Criar Conta</button>
-            </form>
+                    <button type="submit">Criar Conta</button>
+                </form>
+            </div>
         </div>
     );
 }
